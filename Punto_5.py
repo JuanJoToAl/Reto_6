1cff5aeb17540ec9f64c569bb0d5d1ef7c24e655
JuanJoToAl <159054367+JuanJoToAl@users.noreply.github.com>
2024-03-18 20:19:32 -0500

    Add files via upload

--- Punto_5.py
+++ Punto_5.py
@@ -4,7 +4,7 @@ import funciones
 
 if __name__ == "__main__":
     capital = float(input("Ingrese el capital inicial"))
-    ti = float(input("Ingrese la tasa de interés"))
+    ti = float(input("Ingrese la tasa de interés en formato decimal"))
     m = float(input("Ingrese cantidad de meses a la que se realizó el préstamo"))
     prestamo = funciones.prestamo(capital, ti, m)
     print("El valor del préstamo es " + str(round(prestamo, 3)))
