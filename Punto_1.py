4fe8f2f78a5229423bd18a2e958f1d42b1d64e60
JuanJoToAl <159054367+JuanJoToAl@users.noreply.github.com>
2024-03-20 14:56:50 -0500

    Delete Punto_1.py

--- Punto_1.py
+++
@@ -1,9 +0,0 @@
-import funciones
-if __name__ == "__main__":
-    r_esf = float(input("Ingrese el valor del radio de la esfera"))
-    r_cn = float(input("Ingrese el valor del radio del cono"))
-    h_cn = float(input("Ingrese el valor de la altura del cono"))
-    volums = funciones.v_slds(r_esf, r_cn, h_cn)
-    areas = funciones.a_slds(r_esf, r_cn, h_cn)
-    print("La suma de los volúmenes es " + str(round(volums, 3)))
-    print("La suma de las áreas es " + str(round(areas, 3)))
